b14c6f4ab4ab685a3a045ed4ac75f88f8a1bf482
geoffreymgbsn <79371190+geoffreymgbsn@users.noreply.github.com>
2021-02-21 22:02:36 -0600

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ' ';
 let question = '1) Who was the first American woman in space?';
 
 let correctAnswer = 'Sally Ride';
